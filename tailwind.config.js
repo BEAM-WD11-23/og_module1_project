5aff98c777164e31848df8efdde9d5571034b614
Oleksandra G <oleksandra.gindina@students.beaminstitute.org>
2024-02-20 21:09:16 +0100

    created Home page. Started Merch page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,39 @@
 export default {
   content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: {'min': '500px', 'max': '767px'},
+
+      md: {'min': '768px', 'max': '1023px'},
+
+      lg: {'min': '1024px', 'max': '1279px'},
+
+      xl: {'min': '1280px', 'max': '1400px'},
+  
+      xxl: {'min': '1401px'}
+    },
+
+    extend: {
+
+      colors: {
+        headerBlack: 'rgb(15 15 18)',
+        cardsBlack: 'rgb(20 19 26)',
+        cjViolet: 'rgb(102 0 172)',
+        cjHover: 'rgb(59 59 61)'
+      },
+
+      fontFamily: {
+        nunito: ['"Nunito"']
+      },
+
+      boxShadow: {
+        '3xl': '6px 6px 20px 0px rgba(0,0,0,0.57)',
+      }
+
+
+    },
   },
   plugins: [],
 }
+
 
